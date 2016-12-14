6cf80dce49427e0b544a29e0a9d30d3cf42cdb03
Sungho Park <chywoo@gmail.com>
2016-12-15 02:03:56 +0900

    p8. 소수 계산의 고속 버전은 간단했다. 나누기의 회수를 i * i <= n 으로 변경하니, 엄청난 성능이 나왔다. 이렇게 하니 모든 숫자에서 처리 속도가 1초 미만으로 나온다.

--- src/middle/part2/sec5/ga/p9.java
+++ src/middle/part2/sec5/ga/p9.java
@@ -8,12 +8,12 @@ import java.util.*;
 public class p9 {
 
 	public static String input = "1\n" + 
-	"8";
+	"7";
 	static public int T;
 	static public int N, Answer;
 	static public ArrayList<Long> list = new ArrayList<Long>();
-	static public HashSet<Long> primes = new HashSet<Long>();
-	static public long maxPrime;
+//	static public HashSet<Long> primes = new HashSet<Long>();
+//	static public long maxPrime;
 	
 	public static void main(String[] args) {
 		InputStream is = new ByteArrayInputStream(input.getBytes());
@@ -21,13 +21,13 @@ public class p9 {
 		Scanner sc = new Scanner(System.in);
 		
 		T = sc.nextInt();
-		primes.add((long)2);
-		primes.add((long)3);
-		primes.add((long)5);
-		primes.add((long)7);
-		primes.add((long)11);
-		primes.add((long)13);
-		maxPrime = 13;
+//		primes.add((long)2);
+//		primes.add((long)3);
+//		primes.add((long)5);
+//		primes.add((long)7);
+//		primes.add((long)11);
+//		primes.add((long)13);
+//		maxPrime = 13;
 		
 		for(int test_case = 1; test_case <= T; test_case++)
 		{
@@ -76,12 +76,10 @@ public class p9 {
 
 	public static boolean isPrime(long n)
 	{
-		if ( n == 1 ) return false;
-		if ( n == 2 ) return true;
-		if ( n % 2 == 0) return false;
+		if ( n < 2 ) return false;
 			
 		// 소수가 구해지면 이를 저장했다가, 소수로 나누는 것이 더 효과적.
-		for (long i = 3; i < n / 2; i += 2)
+		for (long i = 2; i * i <= n; i ++)
 		{
 			if ( n % i == 0 ) return false;
 		}
@@ -91,47 +89,47 @@ public class p9 {
 	
 	
 	// 망함 ㅠ.ㅠ.. 큰 수가 들어올 경우, 그 수까지 모든 소수를 찾느라 시간 다 까먹음.
-	public static boolean isPrime_enhanced(long n)
-	{
-		if ( n == 2 ) return true;
-		if ( n == 1 || n % 2 == 0 ) return false;
-		
-		// 기존 소수 set에 포함되어 있다면 true.
-		if (primes.contains(n))
-			return true;
-		
-		// 기존 소수 set에 없는데, 소수 set의 최대 소수보다 n이 작으면 소수가 아님
-		if ( n < maxPrime)
-			return false;
-		
-		if ( n > maxPrime) // 누락된 소수를 추가
-		{
-			boolean flag = true;
-			
-			for (long num = maxPrime; num <= n; num += 2)
-			{
-				flag = true;
-				
-				for (long i : primes)
-				{
-					if ( num % i == 0 )
-					{
-						flag = false;
-						break;
-					}
-				}
-				
-				if ( flag )
-				{
-					primes.add(num);
-					maxPrime = num;
-				}
-			}
-		}
-		
-		if (primes.contains(n))
-			return true;
-		
-		return false;
-	}
+//	public static boolean isPrime_enhanced(long n)
+//	{
+//		if ( n == 2 ) return true;
+//		if ( n == 1 || n % 2 == 0 ) return false;
+//		
+//		// 기존 소수 set에 포함되어 있다면 true.
+//		if (primes.contains(n))
+//			return true;
+//		
+//		// 기존 소수 set에 없는데, 소수 set의 최대 소수보다 n이 작으면 소수가 아님
+//		if ( n < maxPrime)
+//			return false;
+//		
+//		if ( n > maxPrime) // 누락된 소수를 추가
+//		{
+//			boolean flag = true;
+//			
+//			for (long num = maxPrime; num <= n; num += 2)
+//			{
+//				flag = true;
+//				
+//				for (long i : primes)
+//				{
+//					if ( num % i == 0 )
+//					{
+//						flag = false;
+//						break;
+//					}
+//				}
+//				
+//				if ( flag )
+//				{
+//					primes.add(num);
+//					maxPrime = num;
+//				}
+//			}
+//		}
+//		
+//		if (primes.contains(n))
+//			return true;
+//		
+//		return false;
+//	}
 }
