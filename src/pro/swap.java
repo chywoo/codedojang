96854de09b4fd0940783ee2d65b7a70f6c32bcaf
Sungho Park <chywoo@gmail.com>
2017-01-03 01:36:39 +0900

    거품 정렬의 swap 줄이기 문제 풀이 코딩 완료

--- src/pro/swap.java
+++ src/pro/swap.java
@@ -2,7 +2,8 @@ package pro;
 
 import util.Timer;
 
-import java.util.ArrayList;
+import java.io.*;
+import java.util.*;
 
 /**
  * Created by chywoo.park on 2017. 1. 2..
@@ -21,7 +22,6 @@ public class swap {
 
     static public int T;
     static public int N, Answer, Count;
-    static public int maxCount;
     static public int[] data;
     static public ArrayList<Integer> list = new ArrayList<>();
 
@@ -34,7 +34,7 @@ public class swap {
 
         for(int test_case = 1; test_case <= T; test_case++)
         {
-            Answer = Count = maxCount = 0;
+            Answer = Count = 0;
             list.clear();
 
             N = sc.nextInt();
@@ -50,37 +50,60 @@ public class swap {
             Answer = solve();
 
             System.out.printf("#%d: %d %d, %fs\n", test_case, Answer, Count, Timer.end());
+            for (int i : list)
+            	System.out.printf("(%d, %d) ",  data[(int)i/1000], data[i % 1000]);
+            
+            System.out.println();
         }
 
         sc.close();
     }
 
     private static int solve() {
-        int swapCount;
-        int pairCount = 0;
+        int swapCount = 0, maxCount = 0;
 
-        int distance = 0; // 두 숫자간의 거리. 거리가 가까우면 swapCount가 작을 수밖에 없음
-
-        for (int last = N - 1; i > 0; i--) {
+        for (int last = N - 1; last > 0; last--) {
             for (int first = 0; first < last; first++) {
-                if (data[first] > data[last]) {
-                    // Swap 개수 계산
-                    swapCount = calcSwap(first, last);
+            	if ( data[first] <= data[last] ) continue;
+            	
+                // SWAP의 개수 계산
+                swapCount = calcSwap(first, last);
 
-                    if (swapCount > maxCount) {
-                        maxCount = swapCount;
-                        pairCount = 1;
-                    }
-                    else if ( swapCount == maxCount )
-                        pairCount++;
+                if (swapCount > maxCount) {
+                    maxCount = swapCount;
+                    Count = 1;
+                    list.clear();
+                    list.add(first * 1000 + last);
+                }
+                else if ( swapCount == maxCount ) {
+                    Count++;
+                    list.add(first * 1000 + last);
                 }
             }
         }
-        return 0;
+        return maxCount;
     }
 
     private static int calcSwap(int first, int last) {
-        return 0;
+        int count = 0;
+        int value;
+
+        value = data[first];
+
+        for(int i = first + 1; i < last; i++)
+        {
+            if ( data[i] < value ) count++;
+            else if ( data[i]  > value ) count--;
+        }
+
+        value = data[last];
+        for(int i = last - 1; i > first; i--)
+        {
+            if ( data[i] > value ) count++;
+            else if ( data[i] < value ) count--;
+        }
+
+        return count;
     }
 
 }
