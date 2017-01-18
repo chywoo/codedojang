2b26cd618df760b84d3d1c8ff4dfb16987e8ba3e
공돌이 <beautifulgondori@gmail.com>
2017-01-19 02:59:40 +0900

    문제 16. 거스름 돈 문제, 수식 계산 형태로 가속화한 풀이 추가

--- src/middle/part2/sec5/ga/p16.java
+++ src/middle/part2/sec5/ga/p16.java
@@ -9,13 +9,16 @@ import java.util.Scanner;
 // 거스름 돈. 문제 8 리모컨과 같은 문제
 public class p16 {
 
-    public static String input = "2 " +
+    public static String input = "3 " +
             "730 " +
             "5 " +
             "10 50 100 500 1250 " +
-            "3000 " +
+            "7300 " +
             "5 " +
-            "10 50 100 500 1250";
+            "10 50 100 500 1250 " +
+            "15002 " +
+            "6 " +
+            "1 10 50 100 500 1000";
 
     static public int T;
     static public int M, N, Answer;
@@ -44,9 +47,28 @@ public class p16 {
 
             Answer = solve1(0, 0);
             System.out.printf("#%d type 1: %d, %fs\n", test_case, Answer, Timer.end());
+
+            Timer.start();
+
+            Answer = solve2();
+            System.out.printf("#%d type 2: %d, %fs\n", test_case, Answer, Timer.end());
         }
 
         sc.close();
+    }
+
+    private static int solve2() {
+        int rest = M;
+        int count = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+           count += rest / data[i];
+           rest = rest % data[i];
+
+           if (rest == 0 ) return count;
+        }
+
+        return count;
     }
 
     private static int solve1(int n, int depth) {
@@ -59,6 +81,8 @@ public class p16 {
         }
         else
         {
+            // 큰 금액의 동전으로 먼저 계산해야 최소의 동전 수에 빨리 도착한다.
+            // 만약 작은 금액의 동전으로 시작하면 효율이 몇 수십 배 떨어진다
             for (int i = N - 1; i >= 0; i--)
                 solve1(n + data[i], depth + 1);
         }
