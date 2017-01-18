27e08636259f37a1cd8e63e7012ab14354212fd0
공돌이 <beautifulgondori@gmail.com>
2017-01-19 02:32:11 +0900

    문제 16. 거스름 돈 문제, 기본 해법으로 풀이 완료

--- src/middle/part2/sec5/ga/p16.java
+++ src/middle/part2/sec5/ga/p16.java
@@ -6,13 +6,21 @@ import java.io.ByteArrayInputStream;
 import java.io.InputStream;
 import java.util.Scanner;
 
-// 계산 오르기
+// 거스름 돈. 문제 8 리모컨과 같은 문제
 public class p16 {
 
-    public static String input = "6 3 4 5 6 7 20";
+    public static String input = "2 " +
+            "730 " +
+            "5 " +
+            "10 50 100 500 1250 " +
+            "3000 " +
+            "5 " +
+            "10 50 100 500 1250";
 
     static public int T;
-    static public int N, Answer;
+    static public int M, N, Answer;
+    static public int[] data;
+    static public int minCoins;
 
     public static void main(String[] args) {
         InputStream is = new ByteArrayInputStream(input.getBytes());
@@ -22,51 +30,39 @@ public class p16 {
         T = sc.nextInt();
 
         for (int test_case = 1; test_case <= T; test_case++) {
+            minCoins = Integer.MAX_VALUE;
+            M = sc.nextInt();
             N = sc.nextInt();
 
+            data = new int[N];
+
+            for (int i = 0; i < N; i++) {
+                data[i] = sc.nextInt();
+            }
+
             Timer.start();
 
-            Answer = 0;
-            solve_tree(0);
-            System.out.printf("#%d type 1: %d -> %d, %fs\n", test_case, N, Answer, Timer.end());
-
-            Timer.start();
-
-            Answer = solve2(N);
-            System.out.printf("#%d type 2: %d -> %d, %fs\n", test_case, N, Answer, Timer.end());
-
-
+            Answer = solve1(0, 0);
+            System.out.printf("#%d type 1: %d, %fs\n", test_case, Answer, Timer.end());
         }
 
         sc.close();
     }
 
-    /**
-     * N개 계단의 값은 처음 1로 시작할 했을 때 N-1 계단의 값과 처음 2로 시작했을 때 N-2개 계단의 합과 같다.
-     * 즉 피보나치 수열이다. F(n) = F(N-1) + F(N-2)
-     * 피보나치 수열의 가속화를 위해서는 Memoiation으로 가능하나 여기서는 하지 않는다.
-     */
-    private static int solve2(int n) {
+    private static int solve1(int n, int depth) {
 
-        if ( n == 1 ) return 1;
-        if ( n == 2 ) return 2;
+        if ( minCoins <= depth || n > M) return minCoins;
 
-        return solve2(n-1) + solve2(n-2);
-    }
-
-    /**
-     * 트리 기반의 알고리즘
-     */
-    private static void solve_tree(int n) {
-        if (n == N) {
-            Answer++;
-            return;
-        } else if (n > N){
-            return;
+        if ( n == M )
+        {
+            minCoins = depth;
+        }
+        else
+        {
+            for (int i = N - 1; i >= 0; i--)
+                solve1(n + data[i], depth + 1);
         }
 
-        solve_tree(n + 1);
-        solve_tree(n + 2);
+        return minCoins;
     }
-
 }
