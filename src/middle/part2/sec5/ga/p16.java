688553aa3f533ffd69ce370a756baf24d491fdcd
Sungho Park <chywoo@gmail.com>
2017-01-20 19:11:37 +0900

    문제 16. 새로운 해결 방법 추가.

--- src/middle/part2/sec5/ga/p16.java
+++ src/middle/part2/sec5/ga/p16.java
@@ -16,14 +16,15 @@ public class p16 {
             "7300 " +
             "5 " +
             "10 50 100 500 1250 " +
-            "15002 " +
+            "11005 " +
             "6 " +
-            "1 10 50 100 500 1000";
+            "5 10 50 100 500 1000";
 
     static public int T;
     static public int M, N, Answer;
     static public int[] data;
     static public int minCoins;
+
 
     public static void main(String[] args) {
         InputStream is = new ByteArrayInputStream(input.getBytes());
@@ -52,23 +53,16 @@ public class p16 {
 
             Answer = solve2();
             System.out.printf("#%d type 2: %d, %fs\n", test_case, Answer, Timer.end());
+
+            Timer.start();
+
+            Answer = solve3(0, 0);
+            System.out.printf("#%d type 3: %d, %fs\n", test_case, Answer, Timer.end());
+
+            System.out.println();
         }
 
         sc.close();
-    }
-
-    private static int solve2() {
-        int rest = M;
-        int count = 0;
-
-        for (int i = N - 1; i >= 0; i--) {
-           count += rest / data[i];
-           rest = rest % data[i];
-
-           if (rest == 0 ) return count;
-        }
-
-        return count;
     }
 
     private static int solve1(int n, int depth) {
@@ -89,4 +83,49 @@ public class p16 {
 
         return minCoins;
     }
+
+    private static int solve2() {
+        int rest = M;
+        int count = 0;
+
+        for (int i = N - 1; i >= 0; i--) {
+           count += rest / data[i];
+           rest = rest % data[i];
+
+           if (rest == 0 ) return count;
+        }
+
+        return count;
+    }
+
+    /**
+     * solve1의 알고리즘에 solve2의 개선 사항을 합한 알고리즘. 그러나 solve2가 더 빠르다. 단지 이런 식으로 가능하다는 공부차원.
+     * @param n
+     * @param depth
+     * @return
+     */
+    private static int solve3(int n, int depth) {
+
+        if ( minCoins <= depth || n > M) return minCoins;
+
+        if ( n == M )
+        {
+            minCoins = depth;
+        }
+        else
+        {
+            int rest = M - n;
+
+            // 큰 금액의 동전으로 먼저 계산해야 최소의 동전 수에 빨리 도착한다.
+            // 만약 작은 금액의 동전으로 시작하면 효율이 몇 수십 배 떨어진다
+            for (int i = N - 1; i >= 0; i--) {
+                int coins = rest / data[i];
+
+                solve1(n + coins * data[i], depth + coins);
+            }
+        }
+
+        return minCoins;
+    }
 }
+
